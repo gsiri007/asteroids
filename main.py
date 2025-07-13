a695792a6890c184b0c754be86ae3b20a717f33e
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2025-07-13 14:16:02 +0530

    FPS adjusted

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    dt = 0
+
+    clock = pygame.time.Clock()
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +19,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)
+        dt /= 1000
 
 if __name__ == "__main__":
     main()
